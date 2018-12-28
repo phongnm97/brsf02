f9cccbe282f5dfd90bed7ff6d07decdf20e17e55
Phong <ngo.minh.phong@framgia.com>
2018-12-28 14:06:58 +0700

    manage user signup, login, logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181227072439) do
+ActiveRecord::Schema.define(version: 20181230161315) do
 
   create_table "activities", force: :cascade do |t|
     t.string "object_type"
@@ -130,6 +130,10 @@ ActiveRecord::Schema.define(version: 20181227072439) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
