f7b1f4cbc798aa70d254f05db527ced2eb4a2ec5
Phong <ngo.minh.phong@framgia.com>
2019-01-10 00:58:18 +0700

    Manage admin dashboard, categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+    namespace :admin do
+      root to: "users#index"
+      resources :categories
+      resources :users
+    end
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
